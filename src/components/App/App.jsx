be2ceea1b0e8d649bb4046716d1c9846229fe65c
Oleksandr <live.gp@gmail.com>
2023-07-23 16:59:41 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Container from './App.styled';
-import Button from '../Button/Button';
 import ImageGallery from '../ImageGallery/ImageGallery';
-import Loader from '../Loader/Loader';
-import ModalWindow from '../Modal/Modal';
 import Search from '../Searchbar/Searchbar';
 
 class App extends Component {
@@ -24,9 +21,6 @@ class App extends Component {
       <Container>
         <Search onSubmit={this.handleSearchSubmit} />
         <ImageGallery search={search} />
-        <Button />
-        <Loader />
-        <ModalWindow />
         <ToastContainer hideProgressBar />
       </Container>
     );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,35 +1,49 @@
 import { Component } from 'react';
 
+import Button from './Button/Button';
 import Container from './ImageGallery.styled';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+import Loader from './Loader/Loader';
+import ModalWindow from './Modal/Modal';
 import onSearch from '../../services/api';
 
 class ImageGallery extends Component {
   state = {
-    gallery: []
+    gallery: null,
+    loading: false
   };
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps) {
     const { search } = this.props;
     if (prevProps.search !== search) {
+      this.setState({ loading: true });
       onSearch(search).then(data => {
-        this.setState({ gallery: data.hits });
+        this.setState({ gallery: data.hits, loading: false });
       });
     }
   }
 
   render() {
-    const { gallery } = this.state;
+    const { gallery, loading } = this.state;
     return (
-      <Container>
-        {gallery.map(image => (
-          <ImageGalleryItem
-            key={image.id}
-            src={image.webformatURL}
-            alt={image.tags}
-          />
-        ))}
-      </Container>
+      <>
+        {loading && <Loader />}
+        {gallery ? (
+          <>
+            <Container>
+              {gallery.map(image => (
+                <ImageGalleryItem
+                  key={image.id}
+                  src={image.webformatURL}
+                  alt={image.tags}
+                />
+              ))}
+            </Container>
+            <Button />
+            <ModalWindow />
+          </>
+        ) : null}
+      </>
     );
   }
 }
