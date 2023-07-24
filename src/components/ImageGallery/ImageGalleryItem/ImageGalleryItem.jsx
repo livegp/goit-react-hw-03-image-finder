5007ef700a2ab069fe7a0af33086fa5fe79f092d
Oleksandr <live.gp@gmail.com>
2023-07-24 22:39:31 +0300

    fix

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
+import PropTypes from 'prop-types';
+
 import Gallery from './ImageGalleryItem.styled';
 
 function ImageGalleryItem({ src, alt, onClick }) {
@@ -12,5 +14,11 @@ function ImageGalleryItem({ src, alt, onClick }) {
     </Gallery>
   );
 }
+
+ImageGalleryItem.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+};
 
 export default ImageGalleryItem;
