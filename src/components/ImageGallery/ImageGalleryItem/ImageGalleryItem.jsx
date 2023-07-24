c962d697d036acaf9a7a5e8a05825d4db68b536e
Oleksandr <live.gp@gmail.com>
2023-07-24 23:42:25 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import PropTypes from 'prop-types';
 
 import Gallery from './ImageGalleryItem.styled';
@@ -10,7 +8,7 @@ function ImageGalleryItem({ id, src, alt, onClick }) {
   };
   return (
     <Gallery>
-      <img src={src} alt={alt} onClick={handleClick} />
+      <img src={src} alt={alt} onClick={handleClick} role="presentation" />
     </Gallery>
   );
 }
